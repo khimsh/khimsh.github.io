03465cf406e5a43d74e459ba988067d760205404
khimsh <i.khimsh@gmail.com>
2019-11-21 10:52:45 +0400

    added mobile navigation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
 // Open/Close Hamburger Menu Icon
+const hamburgerIconTrigger = document.querySelector('.nav-icon');
 const hamburgerIcon = document.querySelector('.icon');
+const nav = document.querySelector('#nav');
 
-hamburgerIcon.addEventListener('click', () => {
+
+hamburgerIconTrigger.addEventListener('click', () => {
     hamburgerIcon.classList.toggle('open');
+    nav.classList.toggle('active');
+    if(nav.classList.contains('active')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'initial';
+    }
+
+    // Hide navigation if one of the links was clicked
+    nav.addEventListener('click', (e) => {
+        if(e.target.tagName.toLowerCase() === 'a') {
+            hamburgerIcon.classList.remove('open');
+            nav.classList.remove('active');
+            document.body.style.overflow = 'initial';
+        }
+    })
 })
